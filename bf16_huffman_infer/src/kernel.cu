5f7c56fcd0f2a4fe90678a87c3dc41f3a23e44a5
lszxb <lszxbgeek@outlook.com>
2025-12-19 15:32:13 +0800

    cleanup kernel code

--- bf16_huffman_infer/src/kernel.cu
+++ bf16_huffman_infer/src/kernel.cu
@@ -34,235 +34,6 @@ static int ceil_div(int a, int b) {
 }
 
 
-template <int batch_size>
-__global__ void gemv_bf16_kernel(
-    const nv_bfloat162* A, const nv_bfloat162* X, nv_bfloat16* Y,
-    int M, int N
-) {
-    N /= 2;
-
-    int thread_id = ((blockIdx.x * blockDim.y + threadIdx.y) * blockDim.x + threadIdx.x) * 2;
-
-    int warp_group_size = warpSize * 2;
-
-    int warp_group_id = thread_id / warp_group_size;
-    int lane_id = thread_id % warp_group_size;
-
-    int stride = N;
-    
-    if (warp_group_id * OP_PER_LANE > M) {
-        return; // no work to do
-    }
-
-    const int boundry_offset = max((warp_group_id + 1) * OP_PER_LANE - M, 0);
-
-    const std::array<nv_bfloat162, 2> *pa = (const std::array<nv_bfloat162, 2> *)&A[(warp_group_id * OP_PER_LANE - boundry_offset) * stride + lane_id];
-    const std::array<nv_bfloat162, 2> *px = (const std::array<nv_bfloat162, 2> *)&X[lane_id];
-
-    std::array<nv_bfloat162, 2> x[batch_size];
-    std::array<nv_bfloat162, 2> a[OP_PER_LANE];
-    float y[batch_size][OP_PER_LANE] = {};
-
-    __syncwarp();
-
-    for (int count = 0, n_iter = N / warp_group_size; count < n_iter; count += 1) {
-        #pragma unroll
-        for (int i = 0; i < batch_size; i++) {
-            x[i] = px[i * (N * 2 / (sizeof(px[0]) / sizeof(nv_bfloat16)))];
-        }
-        const std::array<nv_bfloat162, 2> *npa = pa;
-        #pragma unroll
-        for (int i = 0; i < OP_PER_LANE; i++) {
-            a[i] = *npa;
-            npa += stride / 2;
-        }
-        pa += warpSize;
-        px += warpSize;
-
-
-        float2 v0[batch_size], v1[batch_size];
-        #pragma unroll
-        for (int i = 0; i < batch_size; i++) {
-            v0[i] = __bfloat1622float2(x[i][0]);
-            v1[i] = __bfloat1622float2(x[i][1]);
-        }
-        #pragma unroll
-        for (int i = 0; i < OP_PER_LANE; i++) {
-            auto u0 = __bfloat1622float2(a[i][0]);
-            auto u1 = __bfloat1622float2(a[i][1]);
-            for (int b = 0; b < batch_size; b++) {
-                y[b][i] += (u0.x * v0[b].x + u0.y * v0[b].y) + (u1.x * v1[b].x + u1.y * v1[b].y);
-            }
-        }
-    }
-
-    // warp reduce on y
-    __syncwarp();
-    #pragma unroll
-    for (int b = 0; b < batch_size; b++) {
-        #pragma unroll
-        for (int i = 0; i < OP_PER_LANE; i++) {
-            #pragma unroll
-            for (int j = warpSize / 2; j > 0; j /= 2) {
-                y[b][i] += __shfl_down_sync(0xFFFFFFFF, y[b][i], j);
-            }
-        }
-    }
-
-    // __syncthreads();
-    __syncwarp();
-
-    if (lane_id == 0) {
-        #pragma unroll
-        for (int b = 0; b < batch_size; b++) {
-            #pragma unroll
-            for (int i = 0; i < OP_PER_LANE; i++) {
-                Y[(warp_group_id * OP_PER_LANE - boundry_offset) + i] = __float2bfloat16(y[b][i]);
-            }
-            Y += M;
-        }
-    }
-}
-
-
-void gemv_bf16(
-    const torch::stable::Tensor &A,
-    const torch::stable::Tensor &X,
-    torch::stable::Tensor &Y
-) {
-    int M = A.size(0);
-    int N = A.size(1);
-
-    int num_warps_per_block = 2;
-    auto block_size = dim3(32, num_warps_per_block, 1);
-    int grid_size = ceil_div(M, OP_PER_LANE * num_warps_per_block);
-
-    auto stream = get_tensor_stream(A);
-
-    int batch_size = X.size(0);
-    STD_TORCH_CHECK(batch_size <= 8);
-
-    REP_1_8(
-        b, batch_size,
-        gemv_bf16_kernel<b><<<grid_size, block_size, 0, stream>>>(
-            static_cast<const nv_bfloat162*>(A.data_ptr()),
-            static_cast<const nv_bfloat162*>(X.data_ptr()),
-            static_cast<nv_bfloat16*>(Y.data_ptr()),
-            M, N
-        )
-    );
-}
-
-
-void boxed_gemv_bf16(StableIValue* stack, uint64_t num_args, uint64_t num_outputs) {
-    auto Y = to<torch::stable::Tensor>(stack[2]);
-    gemv_bf16(
-        to<torch::stable::Tensor>(stack[0]),
-        to<torch::stable::Tensor>(stack[1]),
-        Y
-    );
-}
-
-
-struct LUT {
-    uint8_t LUT1[256];
-    uint8_t LUT2[256];
-    uint8_t LUT3[256];
-    uint8_t LUT4[256];
-    uint8_t code_lengths[256];
-};
-
-
-struct decoder{
-    union {
-        uint64_t data;
-        uchar4 v;
-    } state{0};
-    uint8_t remaining_bits = 0;
-
-    __device__ __inline__ uint8_t decode_symbol(
-        const uint32_t* &pae, int warp_group_size,
-        const uint8_t* LUT1, const uint8_t* LUT2, const uint8_t* LUT3, const uint8_t* LUT4,
-        const uint8_t* code_lengths
-    ) {
-        uint8_t symbol;
-
-        if (remaining_bits < 32) {
-            state.data |= uint64_t(*pae) << remaining_bits;
-            pae += warp_group_size;
-            remaining_bits += 32;
-        }
-        
-        if ((symbol = LUT1[state.v.x]) != 255);
-        else if ((symbol = LUT2[state.v.y]) != 255);
-        else if ((symbol = LUT3[state.v.z]) != 255);
-        else if ((symbol = LUT4[state.v.w]) != 255);
-        // else assert(0);
-        auto bitoffset = code_lengths[symbol];
-        state.data >>= bitoffset;
-        remaining_bits -= bitoffset;
-
-        return symbol;
-    }
-
-    __device__ __inline__ uint8_t decode_symbol2(
-        const uint32_t* &pae, int warp_group_size, const LUT *lut
-    ) {
-        uint8_t symbol;
-
-        if (remaining_bits < 32) {
-            // int num = __popc(__activemask());
-            // if (blockIdx.x == 0 && threadIdx.z == 0 && threadIdx.y == 0 && threadIdx.x == 0) {
-            //     printf("active cnt: %d\n", num);
-            // }
-            // TODO: *pae is interleaved load, fix it
-            state.data |= uint64_t(*pae) << remaining_bits;
-            pae += warp_group_size;
-            remaining_bits += 32;
-        }
-        
-        if ((symbol = lut->LUT1[state.v.x]) != 255);
-        else if ((symbol = lut->LUT2[state.v.y]) != 255);
-        else if ((symbol = lut->LUT3[state.v.z]) != 255);
-        else if ((symbol = lut->LUT4[state.v.w]) != 255);
-        // else assert(0);
-        auto bitoffset = lut->code_lengths[symbol];
-        state.data >>= bitoffset;
-        remaining_bits -= bitoffset;
-
-        return symbol;
-    }
-};
-
-
-template <int width> struct vector_type {};
-template <> struct vector_type<1> { using type = uint1; };
-template <> struct vector_type<2> { using type = uint2; };
-template <> struct vector_type<4> { using type = uint4; };
-
-template <typename T, int width>
-union vec {
-    using vector_type = typename vector_type<width>::type;
-    vector_type data;
-    T value[width];
-
-    __device__ __inline__ vec<T, width>& operator=(const vec<T, width>& other) {
-        data = other.data;
-        return *this;
-    }
-
-    __device__ __inline__ vec<T, width>& operator=(vec<T, width>&& other) {
-        data = other.data;
-        return *this;
-    }
-
-    template <typename I>
-    __device__ __inline__ T operator[](I index) {
-        return value[index];
-    }
-};
-
-
 template <int batch_size, typename decoder, typename LUT>
 __device__ __inline__ void
 gemv_bf16_general_kernel(
@@ -436,6 +207,106 @@ gemv_bf16_general_kernel(
 }
 
 
+struct huffman_LUT {
+    uint8_t LUT1[256];
+    uint8_t LUT2[256];
+    uint8_t LUT3[256];
+    uint8_t LUT4[256];
+    uint8_t code_lengths[256];
+};
+
+
+struct huffman_decoder{
+    union {
+        uint64_t data;
+        uchar4 v;
+    } state{0};
+    uint8_t remaining_bits = 0;
+
+    __device__ __inline__ uint8_t decode_symbol(
+        const uint32_t* &pae, int warp_group_size,
+        const uint8_t* LUT1, const uint8_t* LUT2, const uint8_t* LUT3, const uint8_t* LUT4,
+        const uint8_t* code_lengths
+    ) {
+        uint8_t symbol;
+
+        if (remaining_bits < 32) {
+            state.data |= uint64_t(*pae) << remaining_bits;
+            pae += warp_group_size;
+            remaining_bits += 32;
+        }
+        
+        if ((symbol = LUT1[state.v.x]) != 255);
+        else if ((symbol = LUT2[state.v.y]) != 255);
+        else if ((symbol = LUT3[state.v.z]) != 255);
+        else if ((symbol = LUT4[state.v.w]) != 255);
+        // else assert(0);
+        auto bitoffset = code_lengths[symbol];
+        state.data >>= bitoffset;
+        remaining_bits -= bitoffset;
+
+        return symbol;
+    }
+
+    __device__ __inline__ uint8_t decode_symbol2(
+        const uint32_t* &pae, int warp_group_size, const huffman_LUT *lut
+    ) {
+        uint8_t symbol;
+
+        if (remaining_bits < 32) {
+            // int num = __popc(__activemask());
+            // if (blockIdx.x == 0 && threadIdx.z == 0 && threadIdx.y == 0 && threadIdx.x == 0) {
+            //     printf("active cnt: %d\n", num);
+            // }
+            // TODO: *pae is interleaved load, fix it
+            state.data |= uint64_t(*pae) << remaining_bits;
+            pae += warp_group_size;
+            remaining_bits += 32;
+        }
+        
+        if ((symbol = lut->LUT1[state.v.x]) != 255);
+        else if ((symbol = lut->LUT2[state.v.y]) != 255);
+        else if ((symbol = lut->LUT3[state.v.z]) != 255);
+        else if ((symbol = lut->LUT4[state.v.w]) != 255);
+        // else assert(0);
+        auto bitoffset = lut->code_lengths[symbol];
+        state.data >>= bitoffset;
+        remaining_bits -= bitoffset;
+
+        return symbol;
+    }
+};
+
+
+template <typename T, int width>
+union vec {
+    template <int width> struct width_to_vector_type {};
+    template <> struct width_to_vector_type<1> { using type = uint1; };
+    template <> struct width_to_vector_type<2> { using type = uint2; };
+    template <> struct width_to_vector_type<4> { using type = uint4; };
+
+
+    using vector_type = typename width_to_vector_type<width>::type;
+    vector_type data;
+    T value[width];
+
+    __device__ __inline__ vec<T, width>& operator=(const vec<T, width>& other) {
+        data = other.data;
+        return *this;
+    }
+
+    __device__ __inline__ vec<T, width>& operator=(vec<T, width>&& other) {
+        data = other.data;
+        return *this;
+    }
+
+    template <typename I>
+    __device__ __inline__ T operator[](I index) {
+        return value[index];
+    }
+};
+
+
 template <int batch_size>
 __global__ void
 gemv_bf16_huffman_kernel(
@@ -445,7 +316,7 @@ gemv_bf16_huffman_kernel(
     const uint8_t* code_lengths,
     int M, int N, int split_k
 ) {
-    __shared__ LUT sh_LUT;
+    __shared__ huffman_LUT sh_LUT;
 
     ((uint64_t*)sh_LUT.LUT1)[threadIdx.x] = ((const uint64_t*)LUT1)[threadIdx.x];
     ((uint64_t*)sh_LUT.LUT2)[threadIdx.x] = ((const uint64_t*)LUT2)[threadIdx.x];
@@ -453,7 +324,7 @@ gemv_bf16_huffman_kernel(
     ((uint64_t*)sh_LUT.LUT4)[threadIdx.x] = ((const uint64_t*)LUT4)[threadIdx.x];
     ((uint64_t*)sh_LUT.code_lengths)[threadIdx.x] = ((const uint64_t*)code_lengths)[threadIdx.x];
 
-    gemv_bf16_general_kernel<batch_size, decoder, LUT>(
+    gemv_bf16_general_kernel<batch_size, decoder, huffman_LUT>(
         A_rem, A_exp, X, Y,
         offsets,
         &sh_LUT,
@@ -534,7 +405,7 @@ __global__ void huffman_decode_kernel(
     const uint8_t* code_lengths,
     int M, int N, int split_k
 ) {
-    __shared__ LUT sh_LUT;
+    __shared__ huffman_LUT sh_LUT;
 
     ((uint64_t*)sh_LUT.LUT1)[threadIdx.x] = ((const uint64_t*)LUT1)[threadIdx.x];
     ((uint64_t*)sh_LUT.LUT2)[threadIdx.x] = ((const uint64_t*)LUT2)[threadIdx.x];
@@ -565,8 +436,8 @@ __global__ void huffman_decode_kernel(
         uchar4 ar[OP_PER_LANE];
         uchar4 ae[OP_PER_LANE];
 
-        decoder dec;
-        decoder dec2;
+        huffman_decoder dec;
+        huffman_decoder dec2;
 
         __syncwarp();
 
@@ -936,12 +807,13 @@ void boxed_ans_encode(StableIValue* stack, uint64_t num_args, uint64_t num_outpu
 
 
 STABLE_TORCH_LIBRARY_IMPL(bf16_huffman_infer, CUDA, m) {
-    m.impl("gemv_bf16", &boxed_gemv_bf16);
     m.impl("gemv_bf16_huffman", &boxed_gemv_bf16_huffman);
     m.impl("huffman_encode", &boxed_huffman_encode);
     m.impl("huffman_decode", &boxed_huffman_decode);
+
     m.impl("gemv_bf16_ans", &boxed_gemv_bf16_ans);
     m.impl("ans_encode", &boxed_ans_encode);
+    // m.impl("ans_decode", &boxed_ans_decode);
 }
 
 }
